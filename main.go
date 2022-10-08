2e8641de56333ed51694a532ae6eccfe94fc003b
user.email <efg.river@gmail.com>
2022-10-08 12:37:17 +0900

    fix indent

--- main.go
+++ main.go
@@ -4,37 +4,35 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strings"
 	"strconv"
+	"strings"
 )
 
 type AppContext struct {
 	// TODO: replace following with appropriate properties.
-	X	int;
-	Y	int;
-	Result	int;
+	X      int
+	Y      int
+	Result int
 }
 
 type IAppContext interface {
-	Process();
-	PrintResult();
+	Process()
+	PrintResult()
 }
 
 func (p *AppContext) read(sc *bufio.Scanner) (int, int) {
 	sc.Scan()
 	line := strings.Split(sc.Text(), " ")
-        
-        X, err := strconv.Atoi(line[0])
-        if err != nil {
-          // exity
-          os.Exit(1)
-        }
-        Y, err := strconv.Atoi(line[1])
-        if err != nil {
-          // exity
-          os.Exit(1)
-        } 
-        return X, Y
+
+	X, err := strconv.Atoi(line[0])
+	if err != nil {
+		os.Exit(1)
+	}
+	Y, err := strconv.Atoi(line[1])
+	if err != nil {
+		os.Exit(1)
+	}
+	return X, Y
 }
 
 func (p *AppContext) Process() {
@@ -53,17 +51,17 @@ func NewAppContext(sc *bufio.Scanner) *AppContext {
 	context := &AppContext{}
 	// TODO: replace following with appropriate input reading.
 	X, Y := context.read(sc)
-        context.X = X
-        context.Y = Y
+	context.X = X
+	context.Y = Y
 
 	return context
 }
 
 func main() {
 	sc := bufio.NewScanner(os.Stdin)
-        // TODO: make sure required(or suitable) initial / maximum buffer size 
-        buf := make([]byte, 16 * 1024)
-        sc.Buffer(buf, 256 * 1024)
+	// TODO: make sure required(or suitable) initial / maximum buffer size
+	buf := make([]byte, 16*1024)
+	sc.Buffer(buf, 256*1024)
 
 	var context IAppContext = NewAppContext(sc)
 	context.Process()
